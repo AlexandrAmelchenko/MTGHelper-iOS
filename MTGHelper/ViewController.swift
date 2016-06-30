bda8fd62e85866d3d2ad46724ce8424884aa031d
Alexander Amelchenko <alexander.amelchenko@gspd.mobi>
2016-06-30 19:09:34 +0300

    Initial commit

---
+++ MTGHelper/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MTGHelper
+//
+//  Created by Alex Amelchenko on 30/06/16.
+//  Copyright © 2016 Alex Amelchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+
+}
+
